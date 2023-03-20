d865903bc184f06c54c8f4629fabab0769e77f1f
RayanAliDawn <102025889+RayanAliDawn@users.noreply.github.com>
2023-03-20 15:55:38 +0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,48 @@
-import { useState } from "react"
-import { close, logo, menu } from '../assets';
-import { navLinks } from '../constants';
+import { useState } from "react";
 
-function Navbar() {
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+    const [active, setActive] = useState("Home");
     const [toggle, setToggle] = useState(false);
+
     return (
-        <nav className="w-full flex py-6 px-2 justify-between items-center navbar">
-            <img src={logo} alt="business-app " className="w-[124px] h-[32px]" />
+        <nav className="w-full flex py-6 justify-between items-center navbar">
+            <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
             <ul className="list-none sm:flex hidden justify-end items-center flex-1">
                 {navLinks.map((nav, index) => (
                     <li
                         key={nav.id}
-                        className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} `}
-
+                        className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+                            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+                        onClick={() => setActive(nav.title)}
                     >
                         <a href={`#${nav.id}`}>{nav.title}</a>
                     </li>
                 ))}
             </ul>
-            <div className="sm:hidden block ">
-                <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain"
-                    onClick={() => setToggle((prev) => !prev)} />
-                <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0   w-full rounded-xl sidebar `}>
-                    <ul className=" flex flex-col  justify-end items-center flex-1">
+
+            <div className="sm:hidden flex flex-1 justify-end items-center">
+                <img
+                    src={toggle ? close : menu}
+                    alt="menu"
+                    className="w-[28px] h-[28px] object-contain"
+                    onClick={() => setToggle(!toggle)}
+                />
+
+                <div
+                    className={`${!toggle ? "hidden" : "flex"
+                        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+                >
+                    <ul className="sm:hidden block">
                         {navLinks.map((nav, index) => (
                             <li
                                 key={nav.id}
-                                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'} `}
-
+                                className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+                                    } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                                onClick={() => setActive(nav.title)}
                             >
                                 <a href={`#${nav.id}`}>{nav.title}</a>
                             </li>
@@ -37,7 +51,7 @@ function Navbar() {
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
